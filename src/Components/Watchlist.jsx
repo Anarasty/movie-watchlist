1114e279f5c43cde31b04f42a4dd67b8b4dcd2c7
Anarasty <murloc.ninja16@gmail.com>
2025-06-05 21:09:25 +0300

    working with local storage

--- src/Components/Watchlist.jsx
+++ src/Components/Watchlist.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import MovieCard from "./MovieCard";
 
 const Watchlist = () => {
+  const { watchlist } = useContext(GlobalContext);
   return (
-    <div>
-      <h1>Watchlist Page</h1>
-    </div>
-  )
-}
+    <div className="movie-page">
+      <div className="container">
+        <div className="header">
+          <h1 className="heading">My watchlist</h1>
+        </div>
 
-export default Watchlist
+        {watchlist.length > 0 ? (
+          <div className="movie-grid">
+            {watchlist.map((movie) => (
+              <MovieCard movie={movie} type="watchlist" />
+            ))}
+          </div>
+        ) : (
+          <h2 className='no-movies'>No movies in your list</h2>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Watchlist;
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,32 +1,52 @@
-import React, {createContext, useReducer, useEffect} from 'react'
-import AppReducer from './AppReducer';
+import React, { createContext, useReducer, useEffect } from "react";
+import AppReducer from "./AppReducer";
 
 const initialState = {
-    watchlist: [],
-    watched: [],
-}
+  watchlist: localStorage.getItem('watchlist') ? JSON.parse(localStorage.getItem('watchlist')) : [],
+  watched: localStorage.getItem('watched') ? JSON.parse(localStorage.getItem('watched')) : [],
+};
 
+export const GlobalContext = createContext(initialState);
 
-export const GlobalContext = createContext(initialState); 
+export const GlobalProvider = (props) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-export const GlobalProvider = props => {
-     const [state, dispatch] = useReducer(AppReducer, initialState)
+  useEffect(() => {
+    localStorage.setItem('watchlist', JSON.stringify(state.watchlist));
+    localStorage.setItem("watched", JSON.stringify(state.watched))
+  }, [state])
 
-    const addMovieToWatchlist = movie => {
-        dispatch({type: "ADD_MOVIE_TO_WATCHLIST", payload: movie})
-    }
+  const addMovieToWatchlist = (movie) => {
+    dispatch({ type: "ADD_MOVIE_TO_WATCHLIST", payload: movie });
+  };
 
-    return (
-        <GlobalContext.Provider value={{watchlist: state.watchlist, watched: state.watched, addMovieToWatchlist, }}>
-            {props.children}
-        </GlobalContext.Provider>
-    )
-}
+  const removeMovieFromWatchlist = (id) => {
+    dispatch({type: "REMOVE_MOVIE_FROM_WATCHLIST", payload: id})
+  }
+
+  const addMovieToWatched = movie => {
+    dispatch({type: "ADD_MOVIE_TO_WATCHED", payload: movie})
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        watchlist: state.watchlist,
+        watched: state.watched,
+        addMovieToWatchlist,
+        removeMovieFromWatchlist,
+        addMovieToWatched,
+      }}
+    >
+      {props.children}
+    </GlobalContext.Provider>
+  );
+};
 
 // const GlobalState = () => {
 //   return (
 //     <div>
-      
+
 //     </div>
 //   )
 // }
